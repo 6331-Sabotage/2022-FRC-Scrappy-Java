48dec21808c2e3514013394e009802cce2a24222
JWL4402 <j.lebert4402@gmail.com>
2022-03-29 15:49:26 -0400

    added VictorSPXAdapter

---
+++ src/main/java/frc/robot/adapters/VictorSPXAdapter.java
@@ -0,0 +1,48 @@
+package frc.robot.adapters;
+
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+
+public class VictorSPXAdapter implements MotorController {
+
+    private VictorSPX victorSPX;
+
+    private boolean isDisabled = false;
+
+    public VictorSPXAdapter(VictorSPX victorSPX) {
+        this.victorSPX = victorSPX;
+    }
+
+    @Override
+    public void set(double speed) {
+        if (!isDisabled)
+            victorSPX.set(VictorSPXControlMode.PercentOutput, speed);
+    }
+
+    @Override
+    public double get() {
+        return victorSPX.getMotorOutputPercent();
+    }
+
+    @Override
+    public void setInverted(boolean isInverted) {
+        victorSPX.setInverted(isInverted);
+    }
+
+    @Override
+    public boolean getInverted() {
+        return victorSPX.getInverted();
+    }
+
+    @Override
+    public void disable() {
+        isDisabled = true;
+    }
+
+    @Override
+    public void stopMotor() {
+        this.set(0);
+    }
+    
+}
