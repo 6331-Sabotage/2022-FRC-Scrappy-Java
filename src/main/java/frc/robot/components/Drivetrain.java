470b10b0191e3161c8fad630bece82f73c0810eb
JWL4402 <j.lebert4402@gmail.com>
2022-03-31 17:35:36 -0400

    added can ids and fine tuning

--- src/main/java/frc/robot/components/Drivetrain.java
+++ src/main/java/frc/robot/components/Drivetrain.java
@@ -7,24 +7,24 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class Drivetrain {
-    CANSparkMax m_frontLeft = new CANSparkMax(2, MotorType.kBrushless);
-    CANSparkMax m_rearLeft = new CANSparkMax(8, MotorType.kBrushless);
+    CANSparkMax m_frontLeft = new CANSparkMax(2, MotorType.kBrushed);
+    CANSparkMax m_rearLeft = new CANSparkMax(4, MotorType.kBrushed);
     
     MotorControllerGroup m_left = new MotorControllerGroup(m_frontLeft, m_rearLeft);
 
-    CANSparkMax m_frontRight = new CANSparkMax(4, MotorType.kBrushless);
-    CANSparkMax m_rearRight = new CANSparkMax(3, MotorType.kBrushless);
+    CANSparkMax m_frontRight = new CANSparkMax(5, MotorType.kBrushed);
+    CANSparkMax m_rearRight = new CANSparkMax(3, MotorType.kBrushed);
     
     MotorControllerGroup m_right = new MotorControllerGroup(m_frontRight, m_rearRight);
 
     DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
 
     public Drivetrain(double speedMultiplier) {
-        m_left.setInverted(true);
+        m_right.setInverted(true);
         m_drive.setMaxOutput(speedMultiplier);
     }
 
     public void drive(double yAxis, double xAxis) {
-        m_drive.arcadeDrive(yAxis, xAxis);
+        m_drive.arcadeDrive(yAxis, -xAxis);
     }
 }
